2f0d6ca0a4f8c6d645a1a3fb803c5d61aac3d10b
Guilherme Modugno <800041413@PTAM-103995.local>
2020-02-04 08:43:46 -0300

    chore(404): add treat to 404 not found

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const httpStatus = require('http-status');
 require('dotenv').config({ path: 'config.env' });
 
 const { PREFIX, port } = require('./config/api-config');
@@ -18,6 +19,14 @@ app.use(cors());
 app.get(['/', PREFIX], (req, res) => res.send('API Works!'));
 app.use(`${PREFIX}/comments`, commentsRoutes);
 app.use(`${PREFIX}/discoveries`, discoveriesRoutes);
+app.all('*', (req, res, next) => {
+  res
+    .status(httpStatus.NOT_FOUND)
+    .json({
+      statusCode: httpStatus.NOT_FOUND,
+      message: `Rota '${req.originalUrl}' não encontrada`
+    })
+});
 
 app.listen(port, function() {
   console.log(`API is running in http://localhost:${this.address().port}`)
